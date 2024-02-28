3337e065a1ad1d91e670dc2d2f5d523caa967dfc
ycrausaz <yann.crausaz@gmail.com>
2024-02-28 19:30:52 +0100

    Initial commit

---
+++ PM_Safe/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PmSafeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'PM_Safe'
---
+++ PM_Safe/urls.py
@@ -0,0 +1,5 @@
+from django.urls import include
+from django.urls import path
+
+urlpatterns = [
+]
